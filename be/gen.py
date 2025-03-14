2ab937732cddf16655f4aaa724795b4adc42ca2a
KhanhDuy <duyntk74105@gmail.com>
2025-03-14 23:04:06 +0700

    Gen + extract

--- be/gen.py
+++ be/gen.py
@@ -1,13 +1,33 @@
 from google import genai
 from dotenv import load_dotenv
-load_dotenv()
+from copy import deepcopy
 import os
+import json
+import re
+from resume_extractor import ResumeExtractor
 
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+load_dotenv()
 
-response = client.models.generate_content(
-    model=os.getenv("MODEL_NAME"),
-    contents="1 + 1 = ?",
-)
+class GenAgent:
+    _instance = None
 
-print(response.text)
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(GenAgent, cls).__new__(cls)
+            cls._instance.client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+        return cls._instance
+    
+    def generate_content(self, prompt, messages):
+        messages = deepcopy(messages)
+        input_messages = prompt + '\nInput data:\n' + messages
+        response = self.client.models.generate_content(
+            model=os.getenv("MODEL_NAME"),
+            contents=input_messages,
+        )
+        return self.postprocess(response.text)
+    
+    def postprocess(self,output):
+        match = re.search(r'\{.*\}', output, re.DOTALL)
+        json_data = match.group(0)
+        output_dict = json.loads(json_data)
+        return output_dict
--- be/resume_extractor.py
+++ be/resume_extractor.py
@@ -1,37 +1,40 @@
 import fitz
 import docx
 
-def extract_text_from_pdf(pdf_path):
-    text = ""
-    try:
-        doc = fitz.open(pdf_path)
-        for page in doc:
-            text += page.get_text("text") + "\n"
-        doc.close()
-    except Exception as e:
-        print(f"Error when reading PDF: {e}")
-    return text.strip()
+class ResumeExtractor:
+    _instance = None
 
-def extract_text_from_docx(docx_path):
-    text = ""
-    try:
-        doc = docx.Document(docx_path)
-        text = "\n".join([para.text for para in doc.paragraphs])
-    except Exception as e:
-        print(f"Error when reading DOCX: {e}")
-    return text.strip()
-
-
-def extract_resume_text(file_path):
-    if file_path.endswith(".pdf"):
-        return extract_text_from_pdf(file_path)
-    elif file_path.endswith(".docx"):
-        return extract_text_from_docx(file_path)
-    else:
-        raise ValueError("PDF or DOCX only.")
-
-if __name__ == "__main__":
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(ResumeExtractor, cls).__new__(cls)
+        return cls._instance
     
-    file_path = r"D:\Projects\mobiles\recruite_app\be\CV-Frontend-Intern-NguyenThaiKhanhDuy.pdf"
-    text = extract_resume_text(file_path)
-    print(text)
+    def extract_text_from_pdf(self, pdf_path):
+        text = ""
+        try:
+            doc = fitz.open(pdf_path)
+            for page in doc:
+                text += page.get_text("text") + "\n"
+            doc.close()
+        except Exception as e:
+            print(f"Error when reading PDF: {e}")
+        return text.strip()
+
+    def extract_text_from_docx(self, docx_path):
+        text = ""
+        try:
+            doc = docx.Document(docx_path)
+            text = "\n".join([para.text for para in doc.paragraphs])
+        except Exception as e:
+            print(f"Error when reading DOCX: {e}")
+        return text.strip()
+
+
+    def extract_resume_text(self, file_path):
+        if file_path.endswith(".pdf"):
+            return self.extract_text_from_pdf(file_path)
+        elif file_path.endswith(".docx"):
+            return self.extract_text_from_docx(file_path)
+        else:
+            raise ValueError("PDF or DOCX only.")
+    
