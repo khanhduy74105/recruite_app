1f470f0efd3862d8e3c827c59d53e6203bbaed3a
KhanhDuy <duyntk74105@gmail.com>
2025-03-15 15:33:18 +0700

    flutter ap

--- be/app.py
+++ be/app.py
@@ -22,11 +22,21 @@ def sign_up():
         return jsonify({"error": "Email and password are required"}), 400
 
     try:
-        supabase.auth.sign_up({
+        auth = supabase.auth.sign_up({
             'email': email,
             'password': password
         })
-        return jsonify({"message": "User created successfully", "success": True}), 201
+        response = supabase.table('user').select('*').eq('email', email).execute()
+        print(auth.user.email)
+        if not response.data:
+            supabase.table('user').insert({
+                'email': email,
+                "role": 'job_seeker',
+                "full_name": email.split('@')[0],
+            }).execute()
+            return jsonify({"message": "User created successfully", "success": True}), 201
+        else:
+            return jsonify({"message": "User already exists", "success": False}), 400
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
@@ -44,21 +54,47 @@ def sign_in():
             'email': email,
             'password': password
         })
-        response = supabase.table('user').select('*').eq('email', email).execute()
-        if not response.data:
-            user = supabase.table('user').insert({
-                'email': email,
-            }).execute()
-
-            supabase.table('user_info').insert({
-                "user_id": user.data[0]['id'],
-                "role": 'job_seeker',
-                "username": email.split('@')[0],
-            }).execute()
             
         return jsonify({"message": "Authentication successful", "success": True}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 401
+    
+@app.route('/signout', methods=['POST'])
+def sign_out():
+    try:
+        supabase.auth.sign_out()
+        return jsonify({"message": "Sign out successful", "success": True}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 401
+    
+@app.route('/users', methods=['POST'])
+def update_users():
+    try:
+        formData = request.get_json()
+
+        if "id" not in formData:
+            return jsonify({"error": "Missing id"}), 400
+
+        update_data = {key: value for key, value in formData.items() if key != "id" and value}
+
+        if not update_data:
+            return jsonify({"error": "No valid fields to update"}), 400
+
+        response = supabase.table('user') \
+            .update(update_data) \
+            .eq('id', formData["id"]) \
+            .execute()
+
+        return jsonify({"message": "User info updated successfully", "data": response.data}), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
